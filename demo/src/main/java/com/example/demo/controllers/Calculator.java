0346e40cc04a2d70a378de1112a0abf5f8e5f6c5
Ángel Ortiz <anngelo.omz@gmail.com>
2026-02-05 21:49:23 -0600

    calculator with request body

--- demo/src/main/java/com/example/demo/controllers/Calculator.java
+++ demo/src/main/java/com/example/demo/controllers/Calculator.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 @RestController
 public class Calculator {
@@ -25,6 +26,18 @@ public class Calculator {
         return "Result with request param is: " + result;
     }
 
+    // request body
+    // hay que hacer un POST a /calculator/body con un archivo JSON que sea:
+    // { "operation": "sum", "n1": 3, "n2": 4 }
+    @PostMapping("/calculator/body")
+    public String calcBody(@RequestBody Map<String, Object> payload) {
+        String operation = (String) payload.get("operation");
+        double n1 = Double.parseDouble(payload.get("n1").toString());
+        double n2 = Double.parseDouble(payload.get("n2").toString());
+
+        double result = operate(operation, n1, n2);
+        return "Result with request body is: " + result;
+    }
 
     private double operate(String operation, double n1, double n2) {
         return switch (operation.toLowerCase()) {
