131ac392fdb2dd4102255c3c436d6b25396efedd
Ángel Ortiz <anngelo.omz@gmail.com>
2026-02-05 21:17:55 -0600

    calculator with request param

--- demo/src/main/java/com/example/demo/controllers/Calculator.java
+++ demo/src/main/java/com/example/demo/controllers/Calculator.java
@@ -1,10 +1,8 @@
 package com.example.demo.controllers;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 
-@SpringBootApplication
 @RestController
 public class Calculator {
 
@@ -17,6 +15,15 @@ public class Calculator {
         return "Result with path variable is: " + result;
     }
 
+    // query param - request param
+    // por ejemplo poner en el navegadpr: /calculatorR?operation=sum&n1=3&n2=4
+    @GetMapping("/calculatorR")
+    public String calcQuery(@RequestParam String operation,
+                            @RequestParam double n1,
+                            @RequestParam double n2) {
+        double result = operate(operation, n1, n2);
+        return "Result with request param is: " + result;
+    }
 
 
     private double operate(String operation, double n1, double n2) {
